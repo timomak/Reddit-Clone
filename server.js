f68a7e926c93c2346e6dc4c5a4186f9bcb8f47fa
timomak <tmakhlay2@gmail.com>
2018-01-22 11:11:49 -0800

    step 1

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/', (req, res) => {
+  res.render('home', {});
+})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+var exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
