b8d186b3c8f7ac7b43738803a077470115a6a50d
timomak <tmakhlay2@gmail.com>
2018-01-24 20:46:28 -0800

    Revert "Error undefined in terminal"
    This reverts commit 92b6d3b83078649a206f88995fcb1ce1fbc0779b.

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+<<<<<<< HEAD
 //var Post = require('./models/post');
 var exphbs = require('express-handlebars');
 // require('./controllers/posts.js')(app);
+=======
+require('./controllers/posts.js')(app);
+>>>>>>> parent of 92b6d3b... Error undefined in terminal
 
 // Set up
 mongoose.Promise = global.Promise;
@@ -17,9 +21,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, () => console.log('It Loads on port 3000!'))
 app.use(bodyParser.urlencoded({ extended: true }));
 
+<<<<<<< HEAD
 const Post = mongoose.model('Post', {
   title: String
 });
+=======
+
+var Post = require('./models/post');
+
+module.exports = (app) => {
+
+  // CREATE
+  app.post('/posts', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    var post = new Post(req.body);
+>>>>>>> parent of 92b6d3b... Error undefined in terminal
 
 // let posts = [
 //   { title: "Great Review" },
@@ -35,7 +51,23 @@ app.get('/', (req, res) => {
   })
 })
 
+<<<<<<< HEAD
 // NEW
+=======
+app.get('/', (req, res) => {
+  res.render('home', {});
+})
+
+app.post('/posts', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+>>>>>>> parent of 92b6d3b... Error undefined in terminal
 app.get('/posts/new', (req, res) => {
   res.render('posts-new', {});
 })
