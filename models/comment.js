ad1b721b5fce91576c7407844361c9cc49e1f9a0
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-11 10:24:24 -0700

    Revert "Everything Working!"
    This reverts commit 31e924c7d15e82fa34423a8fc1ec5f055c0d7d52.

--- models/comment.js
+++ models/comment.js
@@ -5,9 +5,7 @@ const CommentSchema = new Schema({
 
   author          : { type: Schema.Types.ObjectId, ref: 'User', required: true },
   authorUsername  : { type: String, required: true },
-  content         : { type: String, required: true },
-  postId          : { type: Schema.Types.ObjectId, ref: 'Post', required: true },
-  postTitle       : { type: String }
+  content         : { type: String, required: true }
 
 })
 
--- server.js
+++ server.js
@@ -45,9 +45,15 @@ mongoose.Promise = global.Promise;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+<<<<<<< HEAD
 // mongoose.connect('mongodb://localhost:3000/reddit-clone-tm');
 // mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
 // mongoose.set('debug', true)
+=======
+mongoose.connect('mongodb://localhost:27017/reddit-clone', { useMongoClient: true });
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
+mongoose.set('debug', true)
+>>>>>>> parent of 31e924c... Everything Working!
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.listen(port);
 // app.listen(3000, () => console.log('It Loads on port 3000!'))
@@ -66,7 +72,7 @@ app.post('/posts', (req, res) => {
   if (req.user) {
     var post = new Post(req.body);
     post.author = req.user._id
-    console.log("author: ", req.user);
+    console.log("author: ", req.user._id);
     console.log("author Username: ", req.user.username);
     post.authorUsername = req.user.username
 
@@ -84,8 +90,8 @@ app.post('/posts', (req, res) => {
     return res.status(401); // UNAUTHORIZED
   }
 });
-
 // INDEX
+
 app.get('/', (req, res) => {
   if (req.user) {
     var currentUser = req.user;
@@ -130,7 +136,6 @@ app.get('/n/:subreddit', function(req, res) {
   }
 });
 
-// Page to Create New Post
 app.get('/posts/new', (req, res) => {
   var currentUser = req.user;
   User.findById(currentUser._id).then((currentUsername) => {
@@ -138,7 +143,6 @@ app.get('/posts/new', (req, res) => {
   })
 })
 
-// Post page
 app.get('/posts/:id', function (req, res) {
   if (req.user) {
     var currentUser = req.user;
@@ -160,13 +164,12 @@ app.get('/posts/:id', function (req, res) {
     })
   }
 })
-
 // CREATE Comment
 app.post('/posts/:postId/comments', function (req, res) {
   // INSTANTIATE INSTANCE OF MODEL
   var comment = new Comment(req.body);
-  var titleOfPost;
-
+  comment.author = req.user._id
+  comment.authorUsername = req.user.username
   // SAVE INSTANCE OF Comment MODEL TO DB
   comment.save().then((comment) => {
     return User.findById(req.user._id)
@@ -176,8 +179,6 @@ app.post('/posts/:postId/comments', function (req, res) {
   }).then((comment) => {
     return Post.findById(req.params.postId)
   }).then((post) => {
-    console.log("post title: ", post.title);
-    titleOfPost = post.title;
     post.comments.unshift(comment)
     return post.save()
   }).then((post) => {
@@ -185,10 +186,6 @@ app.post('/posts/:postId/comments', function (req, res) {
   }).catch((err) => {
     console.log(err)
   })
-  comment.author = req.user._id
-  comment.authorUsername = req.user.username
-  comment.postId = req.params.postId
-  comment.postTitle = titleOfPost
 })
 // SIGN UP POST
 app.post('/sign-up', (req, res) => {
@@ -205,7 +202,7 @@ app.post('/sign-up', (req, res) => {
       });
 
     user.save().then((user) => {
-        var token = jwt.sign({ _id: user._id, username: user.username  }, process.env.SECRET, {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, {
           expiresIn: "60 days"
         });
         if (remember == "yes") {
@@ -271,23 +268,16 @@ app.get('/users/:username', (req, res) => {
     User.find({username: req.params.username}).then((array) => {
       var users = array[0];
       console.log("users: ", users);
-      console.log("users comments: ", users.comments);
       Post.find({authorUsername: req.params.username}).then((posts) => {
         console.log("posts: ", posts);
-        Comment.find({authorUsername: req.params.username}).then((comments) => {
-          console.log("comments: ", comments);
-          // Post.findById(comments[0].postId).then((post) => {
-          //   var postTitle = post.title;
-          //   console.log("post Title: ", postTitle);
-          res.render('user-index.handlebars', { comments, currentUsername, users, posts, currentUser })
-          // })
-        })
+        res.render('user-index.handlebars', {  currentUsername, users, posts, currentUser })
       })
     })
   }).catch((err) => {
     console.log(err)
   })
 })
+<<<<<<< HEAD
 
 app.get('/profile', (req, res) => {
   if (req.user) {
@@ -302,3 +292,5 @@ app.get('/profile', (req, res) => {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+=======
+>>>>>>> parent of 31e924c... Everything Working!
